2d09c6270b4a753ef470e6ff5a50c867a8ba96e0
Anna <a.s.markovskaya@gmail.com>
2023-07-01 15:12:12 +0300

    + task 3

---
+++ 3-21-distance_2d/Program.cs
@@ -0,0 +1,19 @@
+﻿// Принимаем координаты 2 точек, выдаем расстояние между ними в двумерном пространстве
+
+double[] pointA = new double[2];
+double[] pointB = new double[2];
+
+Console.WriteLine("Введите координату Х первого числа:");
+pointA[0] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y первого числа:");
+pointA[1] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Х второго числа:");
+pointB[0] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y второго числа:");
+pointB[1] = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Round(Math.Sqrt(Math.Pow((pointB[0] - pointA[0]), 2) + Math.Pow((pointB[1] - pointA[1]), 2)), 3);
+Console.WriteLine(distance);
